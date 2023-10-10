35757dad47fd705caae80ca076b6422367aa7fc1
SeongyongCho <lilisleaves@gmail.com>
2023-10-10 14:26:58 +0900

    initial main

---
+++ app/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.csy.testreposition"
+    compileSdk = 31
+
+    defaultConfig {
+        applicationId = "com.csy.testreposition"
+        minSdk = 29
+        targetSdk = 31
+        versionCode = 1
+        versionName = "1.0"
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.leanback:leanback:1.0.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+}
